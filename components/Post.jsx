605455e430d90ac348e061342e04cb2ed299d27a
Nixon Mbeki <nwokila@gmail.com>
2024-07-11 23:53:42 +0300

    add caption and input box of post component

--- components/Post.jsx
+++ components/Post.jsx
@@ -3,6 +3,7 @@ import { BsThreeDots } from "react-icons/bs";
 import { FaRegHeart } from "react-icons/fa";
 import { BsChatDots } from "react-icons/bs";
 import { FaRegBookmark } from "react-icons/fa6";
+import { HiOutlineEmojiHappy } from "react-icons/hi";
 
 function Post({post}) {
     const {username,img,caption,userImg,id} = post;
@@ -15,12 +16,23 @@ function Post({post}) {
                  
             </div>
             <Image src={img} alt='posted' width={256} height={256}/>
+            {/* Post buttons */}
             <div className="flex justify-between items-center px-4 pt-4">
-                <div className="flex space-x-4" ><FaRegHeart className="btn" size={28}/>
-                <BsChatDots className="btn" size={28}/>
+                <div className="flex space-x-4" >
+                    <FaRegHeart className="btn" size={28}/>
+                    <BsChatDots className="btn" size={28}/>
                 </div>
                 <FaRegBookmark className="btn" size={28}/>
             </div>
+            {/* Post comments */}
+            <p className="p-5 truncated"><span className="mr-2 font-bold">{username}</span>{caption}</p>
+            {/* Post input box */}
+            <form className="flex items-center p-4">
+                <HiOutlineEmojiHappy size={28}/> 
+                <input className="border-none focus:ring-0 flex-1"type="text" placeholder="Enter your comment..."/>
+                <button className="text-blue-400 font-bold">Post</button>
+                </form>
+            
         </div>
     )
 }

fc3e4b25d6913c90c6c1c7f7a55ea273ae8cbf6e
Nixon Mbeki <nwokila@gmail.com>
2024-07-11 23:17:40 +0300

    create header and image section of post component

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
+import { BsThreeDots } from "react-icons/bs";
+
 function Post({post}) {
     const {username,img,caption,userImg,id} = post;
     return (
-        <div>
-            <h2>{username}</h2>
+        <div className="bg-white my-7 border rounded-md">
+            <div className="flex items-center p-5">
+                <Image src={userImg} alt={username} width={48} height={48} className="rounded-full object-cover border p-1 mr-3"/>
+                <p className="font-bold flex-1">{username}</p>
+                <BsThreeDots />
+                 
+            </div>
+            <Image src={img} alt='posted' width={256} height={256}/>
         </div>
     )
 }
--- components/Posts.jsx
+++ components/Posts.jsx
@@ -5,22 +5,22 @@ function Posts() {
         {
             id:"1",
             username:"mikemaignan",
-            userImage: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/800px-Instagram_logo_2022.svg.png",
-            image: "https://images.unsplash.com/photo-1720423413643-363310b689e8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8",
+            userImg: "https://img.freepik.com/premium-photo/graphic-designer-digital-avatar-generative-ai_934475-9292.jpg",
+            img: "https://images.unsplash.com/photo-1720423413643-363310b689e8?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             caption: "Nice car in 7th street"
         },
         {
             id:"2",
             username:"vandevenn",
-            userImage: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/800px-Instagram_logo_2022.svg.png",
-            image: "https://images.unsplash.com/photo-1720649718712-dff5514d5534?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8",
+            userImg: "https://img.freepik.com/premium-photo/graphic-designer-digital-avatar-generative-ai_934475-9292.jpg",
+            img: "https://images.unsplash.com/photo-1720649718712-dff5514d5534?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             caption: "The art of carving"
         },
         {
             id:"3",
             username:"wokilambeki",
-            userImage: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/800px-Instagram_logo_2022.svg.png",
-            image: "https://images.unsplash.com/photo-1720440931331-bdc0e7af2045?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D",
+            userImg: "https://img.freepik.com/premium-photo/graphic-designer-digital-avatar-generative-ai_934475-9292.jpg",
+            img: "https://images.unsplash.com/photo-1720440931331-bdc0e7af2045?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             caption: "Welcome to the savannah"
         }
     ]

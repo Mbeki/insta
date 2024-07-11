3d1022d982e1a385071123c7b5a7db6945d4a09c
Nixon Mbeki <nwokila@gmail.com>
2024-07-11 21:06:56 +0300

    add the menu icons  and profile image at the right side of header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,12 @@
 import Image from "next/image"
 import { CiSearch } from "react-icons/ci";
+import { FaPlus } from "react-icons/fa";
+import { MdHome } from "react-icons/md";
 
 function Header() {
     return (
         
-          <div className="flex items-center justify-between max-w-6xl">
+          <div className="mx-4 xl:mx-auto flex items-center justify-between max-w-6xl">
             <div className="h-24 w-24 relative hidden lg:inline-grid cursor-pointer">
                 <Image
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png?20160616034027"
@@ -23,7 +25,11 @@ function Header() {
                 <div className="absolute top-2 left-2"><CiSearch className="h-5 text-gray-500"/></div>
                 <input type="text" placeholder="Search" className="pl-10 text-sm rounded-md focus:ring-black focus:border-black border-gray-500 bg-gray-50"/>
             </div>
-            <h1>Right side</h1>
+            <div className=" flex space-x-4 items-center">
+                <MdHome size={24} className="h-12 cursor-pointer hover:scale-125 transition-transform duration-200"/>
+                <FaPlus className="h-12 cursor-pointer hover:scale-125 transition-transform duration-200"/>
+            <Image src="https://img.freepik.com/premium-photo/graphic-designer-digital-avatar-generative-ai_934475-9292.jpg" alt="user avatar" width={40} className="hidden md:inline-flex rounded-full h-10 cursor-pointer" height={40}/>
+            </div>
           </div>
        
     )
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:["upload.wikimedia.org","upload.wikimedia.org"]
+        domains:["img.freepik.com","upload.wikimedia.org","upload.wikimedia.org"]
     }
 };
 

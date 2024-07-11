da019f7d180f29bcb0a5ce767f3d4ee4da65e4a9
Nixon Mbeki <nwokila@gmail.com>
2024-07-11 22:25:10 +0300

    style story section and install tailwind-scrollbar

--- components/Story.jsx
+++ components/Story.jsx
@@ -4,8 +4,8 @@ function Story({user}) {
     const {img,username} = user;
     return (
         <div>
-            <Image src={img} alt={username} width={256 } height={256}/>
-            <p>{username}</p>
+            <Image size={56} className="rounded-full p-[1.5px] border-2 cursor-pointer hover:scale-110 transition-transform duration-200 ease-out border-red-500" src={img} alt={username} width={256 } height={256}/>
+            <p className="text-xs w-14 truncate">{username}</p>
             
         </div>
     )

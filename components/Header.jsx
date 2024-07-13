cacc8f4166312a771476b94254939199dfcf0e0b
Nixon Mbeki <nwokila@gmail.com>
2024-07-13 18:07:56 +0300

    get session and modify header component with google data

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,13 @@
+'use client'
 import Image from "next/image"
 import { CiSearch } from "react-icons/ci";
 import { FaPlus } from "react-icons/fa";
 import { MdHome } from "react-icons/md";
+// import { getServerSession } from "next-auth";
+import { useSession,signIn, signOut } from "next-auth/react";
 
-function Header() {
+ function Header() {
+    const {data:session} = useSession();
     return (
         <div className="shadow-sm border-b sticky top-0 bg-white z-30">
           <div className="mx-4 xl:mx-auto flex items-center justify-between max-w-6xl">
@@ -27,8 +31,15 @@ function Header() {
             </div>
             <div className=" flex space-x-4 items-center">
                 <MdHome size={24} className="h-12 cursor-pointer hover:scale-125 transition-transform duration-200 hidden md:inline-flex"/>
-                <FaPlus className="h-12 cursor-pointer hover:scale-125 transition-transform duration-200"/>
-                <Image src="https://img.freepik.com/premium-photo/graphic-designer-digital-avatar-generative-ai_934475-9292.jpg" alt="user avatar" width={40} className=" rounded-full h-10 cursor-pointer" height={40}/>
+                {session? (
+                    <>
+                    <FaPlus className="h-12 cursor-pointer hover:scale-125 transition-transform duration-200"/>
+                    <Image src={session?.user?.image } alt="user avatar" width={40} className=" rounded-full h-10 cursor-pointer" height={40} onClick={()=>signOut()}/>
+                    </>
+                ):(
+                <button onClick={()=>signIn()}>Sign in </button>
+                )}
+                
             </div>
           </div>
           </div>

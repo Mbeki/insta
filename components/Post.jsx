0bf5881410b85d711fbdcfc8747b01564aff1d91
Nixon Mbeki <nwokila@gmail.com>
2024-07-14 11:43:48 +0300

    show comments in the comments section

--- components/Post.jsx
+++ components/Post.jsx
@@ -5,13 +5,24 @@ import { BsChatDots } from "react-icons/bs";
 import { FaRegBookmark } from "react-icons/fa6";
 import { HiOutlineEmojiHappy } from "react-icons/hi";
 import { useSession } from "next-auth/react";
-import { useState } from "react";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { addDoc, collection, onSnapshot, orderBy, query, serverTimestamp } from "firebase/firestore";
 import { db } from "@/firebase";
+import Moment from "react-moment";
 
 function Post({post,id}) {
     const {data:session} =useSession()
     const [comment,setComment] = useState("")
+    const [comments,setComments] = useState([])
+    useEffect(()=>{
+        const unsubscribe = onSnapshot(
+            query(collection(db,"posts",id,"comments"),orderBy("timestamp","desc")),(snapshot)=>{
+                setComments(snapshot.docs)
+            }
+        );
+        
+        return unsubscribe;
+    },[id])
     const {username,image,caption,profileImg} = post;
 
     async function sendComment(event) {
@@ -49,6 +60,18 @@ function Post({post,id}) {
            
             {/* Post comments */}
             <p className="p-5 truncated"><span className="mr-2 font-bold">{username}</span>{caption}</p>
+            {comments.length>0 && (
+                <div className="mx-10 max-h-24 overflow-y-scroll scrollbar-none">
+                    {comments.map(comment =>(
+                        <div key={comment.id} className="flex items-center space-x-2 mb-2">
+                            <Image height={28} width={28} src={comment.data().userImage} className="rounded-full object-cover" alt="user"/>
+                            <p className="font-semibold ">{comment.data().username}</p>
+                            <p className="flex-1 truncate">{comment.data().comment}</p>
+                            <Moment fromNow>{comment.data().timestamp?.toDate()}</Moment>
+                        </div>
+                    ))}
+                </div>
+            )}
             {/* Post input box */}
             {session && <form className="flex items-center p-4">
                 <HiOutlineEmojiHappy size={28}/> 
